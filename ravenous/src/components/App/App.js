a8e3b28a2d87963ffb8f00feb99c0a560b8b207a
Sonia Puertas <epuertas@ucdavis.edu>
2020-06-18 16:09:11 -0700

    added first components

---
+++ ravenous/src/components/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import logo from './logo.svg';
+import ReactDom, { render } from 'react-dom';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+// import Business from './components/Business/Business';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+        
+      </div>
+  
+     
+    );
+
+  }
+
+}
+
+export default App;
---
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './SearchBar.css'
+
+/**
+ * SearchBar allows the communication with the Yelp API
+ */
+
+ const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+ };
+
+
+ class SearchBar extends React.Component{
+     /**
+      *  dynamically create the list items needed to display the sort options 
+      */
+     renderSortByOption () {
+         // return an array ok keys
+         return Object.keys(sortByOptions).map(sortByOption => {
+             let sortedByOptionValue = sortByOptions[sortByOption];
+         return <li key={sortedByOptionValue}>{sortByOption}</li>
+         });
+
+     }
+
+     render(){
+         return(
+            <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+              <ul>
+                {/* Use .renderSortByOptions() to sort the businesses by their options  */}
+                {this.renderSortByOption()}
+              </ul>
+            </div>
+            <div className="SearchBar-fields">
+              <input placeholder="Search Businesses" />
+              <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+              <a>Let's Go</a>
+            </div>
+          </div>
+         );
+     }
+ }
+
+ export default SearchBar;
